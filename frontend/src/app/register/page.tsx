cdd7fbb9fb7617d1a1f1b0a7e125e1ed00351288
Caio Souza <ocaiomoises@gmail.com>
2025-05-23 18:08:26 -0300

    fix: text-black

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,6 +1,6 @@
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center text-white">
+    <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-2xl">Página de Cadastro (em andamento)</h1>
     </div>
   );
