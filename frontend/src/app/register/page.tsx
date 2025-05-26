6cca6f0893231493bc8f148b2830d8d031852573
Breno Eduardo <brenoeduardo.dev@gmail.com>
2025-05-26 20:36:00 -0300

    Campos de input de dados feitos. Modelo inicial definido.

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,7 +1,39 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+import { fromTheme } from "tailwind-merge";
+
 export default function RegisterPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl">Página de Cadastro (em andamento)</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Crie sua conta</CardTitle>
+          <CardDescription>Preencha todos os campos</CardDescription>
+        </CardHeader>
+        <CardContent className="">
+          <p>Digite seu nome de usuário</p>
+          <Input type="default" placeholder="Nome"/>
+          <p>Digite seu email</p>
+          <Input type="email" placeholder="Email"/>
+          <p>Digite sua senha</p>
+          <Input type="password" placeholder="Senha"/>
+          <p>Confirme sua senha</p>
+          <Input type="password" placeholder="Confirme a senha"/>
+        </CardContent>
+        <CardFooter>
+          <Button variant={"default"}>Registrar</Button>
+        </CardFooter>
+      </Card>
+
     </div>
   );
 }
