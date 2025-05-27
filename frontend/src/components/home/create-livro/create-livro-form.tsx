8c0db5cd8c6e10c486bb165ba92d70d20d0c4401
Caio Souza <160979584+cmoiss@users.noreply.github.com>
2025-05-27 14:45:24 +0000

    feat: Validação e captura dos dados ao cadastrar

--- frontend/src/components/home/create-livro/create-livro-form.tsx
+++ frontend/src/components/home/create-livro/create-livro-form.tsx
@@ -1,9 +1,103 @@
-import React from 'react'
+"use client";
+
+import { useForm } from "react-hook-form";
+import { Input } from "../../ui/input";
+import { Label } from "../../ui/label";
+import { Textarea } from "../../ui/textarea";
+import Gender from "./genero";
+import {
+  CreateLivroSchema,
+  CreateLivroType
+} from "@/components/home/create-livro/create-livro-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function CreateLivroForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<CreateLivroType>({
+    resolver: zodResolver(CreateLivroSchema)
+  });
+
+  const onSubmit = async (data: CreateLivroType) => {
+    /*
+     * alert(
+      data.titulo +
+        "\n " +
+        data.autor +
+        "\n " +
+        data.isbn +
+        "\n " +
+        data.quantidade
+    );
+     */
+    
+    // await new Promise((resolve) => setTimeout(resolve, 2000)); // Simula um consumo de API
+    console.log("Dados do livro entiados:", data);
+  };
+
   return (
     <div>
-        <form action=""></form>
+      <form onSubmit={handleSubmit(onSubmit)} id="create-livro-form">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="titulo">Título</Label>
+            <Input id="titulo" {...register("titulo")} />
+            {errors.titulo && (
+              <p className="text-red-500">{errors.titulo.message}</p>
+            )}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="autor">Autor</Label>
+            <Input id="autor" {...register("autor")} />
+            {errors.autor && (
+              <p className="text-red-500">{errors.autor.message}</p>
+            )}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="sinopse">Sinopse</Label>
+            <Textarea id="sinopse" {...register("sinopse")} />
+            {errors.sinopse && (
+              <p className="text-red-500">{errors.sinopse.message}</p>
+            )}
+          </div>
+
+          <div className="flex flex-col">
+            <Gender />
+          </div>
+
+          <div className="grid grid-cols-5 gap-3 items-center">
+            <Label htmlFor="isbn" className="text-right">
+              ISBN
+            </Label>
+            <Input id="isbn" className="col-span-3" {...register("isbn")} />
+            {errors.isbn && (
+              <p className="text-red-500 col-span-5 text-right">
+                {errors.isbn.message}
+              </p>
+            )}
+          </div>
+
+          <div className="grid grid-cols-5 gap-3 items-center">
+            <Label htmlFor="quantidade" className="text-right">
+              Quantidade
+            </Label>
+            <Input
+              id="quantidade"
+              type="number"
+              {...register("quantidade", { valueAsNumber: true })}
+            />
+            {errors.quantidade && (
+              <p className="text-red-500 col-span-5 text-right">
+                {errors.quantidade.message}
+              </p>
+            )}
+          </div>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
---
+++ frontend/src/components/home/create-livro/create-livro-modal.tsx
@@ -0,0 +1,44 @@
+import { Button } from "../../ui/button";
+import {
+  DialogHeader,
+  DialogFooter,
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from "../../ui/dialog";
+import CreateLivroForm from "./create-livro-form";
+
+export default function CreateLivroModal() {
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="cursor-pointer">Cadastrar livro</Button>
+        </DialogTrigger>
+
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Cadastrar livro</DialogTitle>
+            <DialogDescription>
+              Insira os dados nos campos abaixo.
+            </DialogDescription>
+          </DialogHeader>
+
+          <CreateLivroForm />
+
+          <DialogFooter>
+            <DialogClose>
+              <Button variant={"ghost"} type="button">
+                Cancelar
+              </Button>
+            </DialogClose>
+            <Button type="submit" form="create-livro-form">Salvar</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
