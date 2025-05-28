9d7fa29b5155b6f3bf6c6981522da10d3493cf83
Breno Eduardo <brenoeduardo.dev@gmail.com>
2025-05-27 23:34:26 -0300

    Começo de implementação da lógica de preenchimento do forms

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -7,30 +9,61 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { Input } from "@/components/ui/input";
 
 import { fromTheme } from "tailwind-merge";
 
-export default function RegisterPage() {
+import { useState } from "react";
+
+export default function Cadastro() {
+
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (senha !== confirmarSenha) {
+      alert("As senhas não conicidem!");
+      return;
+    }
+
+    setDialogOpen(true);
+
+    console.log({nome, email, senha, confirmarSenha});
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <Card>
+      <Card className="">
         <CardHeader>
           <CardTitle>Crie sua conta</CardTitle>
           <CardDescription>Preencha todos os campos</CardDescription>
         </CardHeader>
         <CardContent className="">
-          <p>Digite seu nome de usuário</p>
-          <Input type="default" placeholder="Nome"/>
-          <p>Digite seu email</p>
-          <Input type="email" placeholder="Email"/>
-          <p>Digite sua senha</p>
-          <Input type="password" placeholder="Senha"/>
-          <p>Confirme sua senha</p>
-          <Input type="password" placeholder="Confirme a senha"/>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <Input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} required/>
+            <Input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+            <Input type="password" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} required/>
+            <Input type="password" placeholder="Confirme sua senha" value={confirmarSenha} onChange={(e) => setConfirmarSenha(e.target.value)}/>
+
+            <Button type="submit" className="w-full cursor-pointer" variant={"default"}>Registrar</Button>
+          </form>
         </CardContent>
         <CardFooter>
-          <Button variant={"default"}>Registrar</Button>
         </CardFooter>
       </Card>
 
